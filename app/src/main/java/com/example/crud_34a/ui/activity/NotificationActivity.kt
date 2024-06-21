86a5f558198041025db298c0b4a89dc7adca6f46
sprajapati123 <sandisprajapati52@gmail.com>
2024-06-21 11:46:50 +0545

    notifications

---
+++ app/src/main/java/com/example/crud_34a/ui/activity/NotificationActivity.kt
@@ -0,0 +1,52 @@
+package com.example.crud_34a.ui.activity
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crud_34a.R
+import com.example.crud_34a.databinding.ActivityNotificationBinding
+
+class NotificationActivity : AppCompatActivity() {
+    lateinit var notificationBinding: ActivityNotificationBinding
+    var CHANNEL_ID = "1"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        notificationBinding = ActivityNotificationBinding.inflate(layoutInflater)
+        setContentView(notificationBinding.root)
+
+        notificationBinding.btnNotification.setOnClickListener {
+            showNotification()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun showNotification() {
+        var builder = NotificationCompat.Builder(this@NotificationActivity, CHANNEL_ID)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            var channel = NotificationChannel(
+                CHANNEL_ID, "My channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            var manager: NotificationManager = getSystemService(NOTIFICATION_SERVICE)
+                    as NotificationManager
+
+            manager.createNotificationChannel(channel)
+
+        }
+
+
+    }
+}
